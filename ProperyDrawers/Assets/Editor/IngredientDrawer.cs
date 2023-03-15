cf72d3f6826731a14c0af08115dff9bbd8ca39f6
vicmo123 <vicmo26@gmail.com>
2023-03-15 11:09:56 -0400

    Better tool

--- ProperyDrawers/Assets/Editor/IngredientDrawer.cs
+++ ProperyDrawers/Assets/Editor/IngredientDrawer.cs
@@ -6,22 +6,21 @@ using propertyDrawerTool;
 [CustomPropertyDrawer(typeof(Ingredient))]
 public class IngredientDrawer : PropertyDrawer
 {
-    int extraLines { get; set; } = 0;
+    float totalPropertyHeight { get; set; } = 0;
     float width = 100;
     float height = EditorGUIUtility.singleLineHeight;
     float spacing = 5f;
 
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-
-        //extraLines = PropertyDrawerLayoutTool.GenerateProperty(label, position, spacing, new PropertyData(property.FindPropertyRelative("amount"), width, height2), new PropertyData(property.FindPropertyRelative("unit"), width, height), new PropertyData(property.FindPropertyRelative("name"), width, height2), new PropertyData(property.FindPropertyRelative("amount"), width, height), new PropertyData(property.FindPropertyRelative("unit"), width, height), new PropertyData(property.FindPropertyRelative("name"), width, height));
-        extraLines = PropertyDrawerLayoutTool.GenerateProperty(label, position, spacing, PropertyDrawerLayoutTool.GetPropertyFields(property, "Ingredient", width, height));
+        //lineSpacing = PropertyDrawerLayoutTool.GenerateProperty(label, position, spacing, PropertyDrawerLayoutTool.GetPropertyFields(property, "Ingredient", width, height));
+        totalPropertyHeight = PropertyDrawerLayoutTool.GenerateProperty(label, position, spacing, PropertyDrawerLayoutTool.GetPropertyFields(property, typeof(Ingredient), width, height));
     }
 
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        float extraHeight = EditorGUIUtility.singleLineHeight * extraLines + EditorGUIUtility.standardVerticalSpacing * extraLines;
-        return base.GetPropertyHeight(property, label) + extraHeight;
+        return totalPropertyHeight;
+        //return (base.GetPropertyHeight(property, label) + spacing) * numLines;
     }
 }
--- ProperyDrawers/Assets/Editor/PropertyDrawerLayoutTool.cs
+++ ProperyDrawers/Assets/Editor/PropertyDrawerLayoutTool.cs
@@ -8,7 +8,7 @@ namespace propertyDrawerTool
 {
     public static class PropertyDrawerLayoutTool
     {
-        public static int GenerateProperty(GUIContent mainLabel, Rect position, float spacing, params PropertyData[] content)
+        public static float GenerateProperty(GUIContent mainLabel, Rect position, float spacing, params PropertyData[] content)
         {
             EditorGUI.BeginProperty(position, mainLabel, content[0].property);
 
@@ -19,58 +19,71 @@ namespace propertyDrawerTool
             float offsetX = 0f;
             float offsetY = 0f;
             float rowHeight = 0f;
-            int extraLines = 0;
+            float totalPropertyHeight = 0f;
 
             for (int i = 0; i < content.Length; i++)
             {
-                Rect rect = position.Offset(offsetX, offsetY).ChangeSize(content[i].width, content[i].height);
-
-                EditorGUI.PropertyField(rect, content[i].property, content[i].label, false);
-
-                if (rect.height > rowHeight)
-                {
-                    rowHeight = rect.height;
-                }
-
-                offsetX += content[i].width + spacing;
-                if (offsetX > position.width)
+                if (offsetX + (content[i].width / 2) > position.width)
                 {
                     offsetY += rowHeight + spacing;
                     offsetX = 0;
                     rowHeight = 0;
-                    extraLines++;
                 }
+
+                Rect rect = position.Offset(offsetX, offsetY).ChangeSize(content[i].width, content[i].height);
+                
+                if (rect.height > rowHeight)
+                {
+                    rowHeight = rect.height;
+                    totalPropertyHeight += rowHeight + spacing;
+                }
+
+                EditorGUI.PropertyField(rect, content[i].property, content[i].label, false);
+                offsetX += content[i].width + spacing;
             }
 
             EditorGUI.indentLevel = indent;
             EditorGUI.EndProperty();
 
-            return extraLines;
+            return totalPropertyHeight;
         }
 
-        public static Rect Offset(this Rect rect, float x = 0, float y = 0)
+        public static Rect Offset(this Rect rect, float x, float y)
         {
             return new Rect(rect.x + x, rect.y + y, rect.width, rect.height);
         }
 
-        public static Rect ChangeSize(this Rect rect, float width = 0, float height = 0)
+        public static Rect ChangeSize(this Rect rect, float width, float height)
         {
             return new Rect(rect.x, rect.y, width, height);
         }
 
         public static PropertyData[] GetPropertyFields(SerializedProperty serializable, string typeName, float mainWidth, float mainHeight)
         {
-            List<PropertyData> props = new List<PropertyData>();
-
             System.Type type = GetSystemTypeWithString(typeName);
-            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
 
-            foreach (var field in fields)
+            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            PropertyData[] props = new PropertyData[fields.Length];
+
+            for (int i = 0; i < fields.Length; i++)
             {
-                props.Add(new PropertyData(serializable.FindPropertyRelative(field.Name), mainWidth, mainHeight));
+                props[i] = new PropertyData(serializable.FindPropertyRelative(fields[i].Name), mainWidth, mainHeight);
             }
 
-            return props.ToArray();
+            return props;
+        }
+
+        public static PropertyData[] GetPropertyFields(SerializedProperty serializable, System.Type type, float mainWidth, float mainHeight)
+        {
+            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            PropertyData[] props = new PropertyData[fields.Length];
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+                props[i] = new PropertyData(serializable.FindPropertyRelative(fields[i].Name), mainWidth, mainHeight/*, new GUIContent(fields[i].Name)*/);
+            }
+
+            return props;
         }
 
         public static System.Type GetSystemTypeWithString(this string toType)
